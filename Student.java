9b94d6175d35137c272c361893e156f5ff9101b9
Angel Barajas <147221809+Ang-el12@users.noreply.github.com>
2024-02-18 21:49:05 -0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -3,7 +3,11 @@ package mystudent;
 public class Student {
     String firstName;
     String lastname;
+    int age;
     double gpa;
     String major;
     String city;
+    String state;
+    int idNumber;
+    String year;
 }
